fac36586ff8315908f7f9e2909ab908ef6045ab8
Henrique Eidt <henriqueidt@gmail.com>
2024-03-17 16:53:11 -0300

    include typedoc-plugin-markdown

--- poc-typedoc/scripts/generate-docs.ts
+++ poc-typedoc/scripts/generate-docs.ts
@@ -1,4 +1,5 @@
 const TypeDoc = require("typedoc");
+// import TypeDoc from "typedoc";
 
 async function main() {
   // Application.bootstrap also exists, which will not load plugins
@@ -6,11 +7,16 @@ async function main() {
   // TypeDoc's tsconfig.json/package.json/typedoc.json option readers
   const app = await TypeDoc.Application.bootstrapWithPlugins({
     entryPoints: ["src/components/**/*.tsx"],
+    // Found typedoc-plugin-markdown which can be used to generate the docs as markdown
+    // We can use markdown in storybook mdx
+    plugin: ["typedoc-plugin-markdown"],
   });
 
   const project = await app.convert();
 
   if (project) {
+    // Project contains the three of files. Might be able to use it to generate the html for each component
+    console.log(project);
     // Project may not have converted correctly
     const outputDir = "docs";
 
